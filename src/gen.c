03605edeaa591f0a751b6562ddfaae39af8f2cd3
aidan s <avstanf1@go.olemiss.edu>
2026-04-28 02:16:20 -0500

    kept modifying

--- src/gen.c
+++ src/gen.c
@@ -25,6 +25,7 @@ int *sudoku_init(const int dim)
 	// basically just allocate a pointer to a malloced array
 	int *ret = (int *)calloc(dim * dim, sizeof(int));
 	check = (int *)calloc(3 * dim, sizeof(int));
+	memset(check, -1, 3 * dim * sizeof(int));
 
 	// check pointer
 	assert(ret && "ret malloc in sudoku_init failed\n");
@@ -44,8 +45,8 @@ void sudoku_fill(int *s)
 {
 	const int conf_dim_sq = conf_dim*conf_dim;
 	const int conf_dim_sqr = (int)sqrt(conf_dim);
-	int temp_ind, temp_sum = 1, row, col, blk;
-	bool is_valid = false;
+	int temp_ind, temp_sum = 0, row, col, blk;
+	bool not_valid = false;
 
 	// check pointer s
 	assert(s && "sudoku array not initialized in sudoku_fill\n");
@@ -53,7 +54,6 @@ void sudoku_fill(int *s)
 	// this is gonna be stupid bro
 	for (int i = 0; i < 9; i++) {
 		// get a random array pos until we find a number that works :(
-		temp_sum += (i+1);
 		for (int j = 0; j < 9; j++) {
 			do {
 				temp_ind = rand() % conf_dim_sq; // index in original array
@@ -63,15 +63,17 @@ void sudoku_fill(int *s)
 				blk = (col/conf_dim_sqr) + ((row / conf_dim_sqr) * conf_dim_sqr);
 
 				// we do it this way because of how check is organized
-				is_valid = (check[row] == temp_sum) ||
-						   (check[conf_dim+col] == temp_sum) ||
-						   (check[2*conf_dim+blk] == temp_sum);
-				// printf("row: %d col: %d blk: %d\n", row, col, blk);
-			} while (is_valid);
-			s[temp_ind] = i+1;
+				not_valid = (s[temp_ind] != 0) || 
+							((check[row] == temp_sum) ||
+						    (check[conf_dim+col] == temp_sum) ||
+						    (check[2*conf_dim+blk] == temp_sum));
+			} while (not_valid);
+
+			s[col + (row * conf_dim)] = i+1;
 			check[row] = check[conf_dim+col] = check[2*conf_dim+blk] = temp_sum;
 			printf("%d at row %d col %d blk %d\n", i+1, row, col, blk);
 		}
+		temp_sum += (i+1);
 	}
 }
 
@@ -83,14 +85,16 @@ int main(void)
 	// print the array out here like this
 	for (int i = 0; i < 9; i++) {
 		for (int j = 0; j < 9; j++) {
-			if (s_arr[(9*i)+j] == 0) {
-				printf("  ");
-			} else {
 			printf("%d ", s_arr[(9*i)+j]);
-			}
 		}
 		printf("\n");
 	}
+
+	for (int i = 0; i < 27; i++)
+	{
+		printf("%d ", check[i]);
+	}
+	printf("\n");
 
 	free(s_arr);
 	free(check);
